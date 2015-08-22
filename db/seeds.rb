90eb8626108ce3948016b8e6d61058caf8f3e9b1
jorliano <jorliano@hotmail.com>
2015-08-22 16:35:42 -0300

    devise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ Usuario.create(email: 'admin@vendas.com', password: '12345678')
